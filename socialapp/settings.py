5030e257847029eae039e259a40727ea1e5e1401
Mr-Bagish <124681431+Mr-Bagish@users.noreply.github.com>
2026-04-22 16:40:02 +0530

    Update settings.py

--- socialapp/settings.py
+++ socialapp/settings.py
@@ -21,10 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# 🔐 SECURITY
-SECRET_KEY = os.environ.get("SECRET_KEY")
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-8lpsm=py+_@k4l7tp(7hzgdf^f3p@9!7e+j&e*tcn0hr%&6lk#'
 
-DEBUG = os.environ.get("DEBUG") == "True"
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
